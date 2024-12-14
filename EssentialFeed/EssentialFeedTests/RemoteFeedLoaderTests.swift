70f24744884f29bde55c67e7f8c7a316b023b284
Nebojsa Gujanicic <nebojasa@gmail.com>
2024-12-14 23:40:31 +0100

    Using Singletons for HTTPClient, test Green

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,17 +8,48 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
     var requestedURL: URL?
+    
+    private init() {}
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
+    
+    var client: HTTPClient!
+    var sut: RemoteFeedLoader!
+    
+    override func setUp() {
+        super.setUp()
+        client = HTTPClient.shared
+        sut = RemoteFeedLoader()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        client = nil
+        sut = nil
+    }
+    
     func test_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        // Given
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        // When
+        sut.load()
+        // Then
+        XCTAssertNotNil(client.requestedURL)
     }
 }
