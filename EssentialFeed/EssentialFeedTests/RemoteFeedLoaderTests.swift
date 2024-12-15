2dc8255e72d69d75339dba95399a63a43a45ba38
Nebojsa Gujanicic <nebojasa@gmail.com>
2024-12-15 23:54:03 +0100

    Replace stubing by capturing values instead. Still using HTTPClientSpy

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -40,10 +40,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
         // Given
         let url = URL(string: "https://a-given-url.com")!
         let (sut,client) = makeSUT(url: url)
-        client.error = NSError(domain: "Test", code: 0)
-        // When
+        
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
+        // When
+        
         // Than
         XCTAssertEqual(capturedErrors, [.conectivity])
     }
@@ -60,15 +64,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         
         func get(
             from url: URL,
             completion: @escaping (Error) -> Void
         ) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
