cb97e6549822b87954f72da02d949583e327af2b
Nebojsa Gujanicic <nebojasa@gmail.com>
2024-12-27 12:03:16 +0100

    Improved readablity with helper methhod for completing with Error

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -45,7 +45,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load { capturedErrors.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         // When
         
         // Than
@@ -72,6 +72,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
         ) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 
