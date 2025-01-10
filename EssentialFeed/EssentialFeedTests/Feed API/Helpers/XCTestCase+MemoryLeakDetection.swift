123a84f5c928b4bed0db545c37fd97d9f08f4800
Nebojsa Gujanicic <nebojasa@gmail.com>
2025-01-10 20:28:56 +0100

    Added memory leaks tracking for components in integration.

--- EssentialFeed/EssentialFeedTests/Feed API/Helpers/XCTestCase+MemoryLeakDetection.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/Helpers/XCTestCase+MemoryLeakDetection.swift
@@ -7,8 +7,8 @@
 
 import XCTest
 
-extension XCTestCase {
-    func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+public extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance,"Instance should have been dealocated! Potential Memory leak.", file: file, line: line)
         }
