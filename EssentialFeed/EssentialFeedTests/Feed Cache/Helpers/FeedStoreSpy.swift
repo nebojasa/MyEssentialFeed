c1af656c2d049f1fe827ac14d1bf5b6288b8f7b9
Nebojsa Gujanicic <nebojasa@gmail.com>
2025-08-06 07:50:09 +0200

    Extract `FeedCacheSpy` into a shared scope to remove duplication

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,47 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeed
+//
+//  Created by Nebojša Gujaničić on 6. 8. 2025..
+//
+import Foundation
+import EssentialFeed
+
+class FeedStoreSpy: FeedStore {
+    var insertions = [(items: [FeedImage], timestamp: Date)]()
+    
+    enum ReceivedMessage: Equatable {
+        case deleteCacheFeed
+        case insert([LocalFeedImage], Date)
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    var deletionCompletions = [DeletionCompletion]()
+    var insertionCompletions = [InsertionCompletion]()
+    
+    func deleteCashedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCacheFeed)
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+}
