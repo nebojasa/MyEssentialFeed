35764ce68ae428a4f650a3a6e5bd7948b4fa29d6
Nebojsa Gujanicic <nebojasa@gmail.com>
2025-09-14 15:48:24 +0200

    Validate cache command does not delete less than seven days old cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCase.swift
@@ -34,6 +34,19 @@ final class ValidateFeedCacheUseCase: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_validate_doesNotDeleteLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT { fixedCurrentDate }
+        
+        sut.validateCache()
+        
+        store.completeRetrieval(with: feed.localItems, timestamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     
     // MARK: - Helpers
     
@@ -50,5 +63,29 @@ final class ValidateFeedCacheUseCase: XCTestCase {
     
     private func anyNSError() -> NSError {
         NSError(domain: "any error", code: 0)
+    }
+    
+    private func uniqueImageFeed() -> (models: [FeedImage], localItems: [LocalFeedImage]) {
+        let items = [uniqueImage(), uniqueImage()]
+        let localFeedItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (models: items, localItems: localFeedItems)
+    }
+    
+    private func uniqueImage() -> FeedImage {
+        FeedImage(id: UUID(), description: "any string", location: nil, url: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
