705aef71ddd22287171ec2b961dcff9c31041cf0
Nebojsa Gujanicic <nebojasa@gmail.com>
2025-10-02 15:45:20 +0200

    Maybe to early but here important to compare concrete value. This needs to be double checked

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -19,17 +19,31 @@ class CodableFeedStoreTests: XCTestCase {
     func test_retreive_deliversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
         let exp = expectation(description: "Wait for cache retreival")
+        var retrivedResult: RetriveCachedFeedResult?
         
         sut.retrieve { result in
             switch result {
             case .empty:
-                break
+                retrivedResult = result
             default :
                 XCTFail("Expected empty result, got \(result) instead.")
             }
             exp.fulfill()
         }
-        
         wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(retrivedResult, .empty)
+    }
+}
+
+extension RetriveCachedFeedResult: @retroactive Equatable {
+    public static func == (lhs: RetriveCachedFeedResult, rhs: RetriveCachedFeedResult) -> Bool {
+        switch (lhs, rhs) {
+        case (.empty, .empty):
+            return true
+        case (.found(feed: let feed, timestamp: let timestamp), .found(feed: let feed2, timestamp: let timestamp2)):
+            return feed == feed2 && timestamp == timestamp2
+        default:
+            return false
+        }
     }
 }
